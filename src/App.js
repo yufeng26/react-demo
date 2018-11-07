51e9660654d5d52ce568aad91e308abf73ecd913
chenhui <1945199284@qq.com>
2018-11-07 18:30:45 +0800

    'l'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// 此处应用到ES6解构赋值 const { Component } = react.conponent
+import Todolist from './conponent/Todolist';
+class App extends Component {
+  render() {
+    return (
+      // jsx语法，js中写的标签
+      <Todolist></Todolist>
+    );
+  }
+}
+
+export default App;
---
+++ src/conponent/Todolist.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Todolist extends Component {
+  render() {
+    return (
+      // 可以用Fragment当做占位符标签，也是react中的  
+      <div className="todoList">
+         todoList
+      </div>
+    );
+  }
+}
+
+export default Todolist;
